a001c22adb48cc298ad951938b07eaf4e43e9951
modernscape <modernscape@gmail.com>
2022-08-06 15:38:28 +0900

    n

--- data.js
+++ data.js
@@ -1,28 +1,12 @@
 var exItems = [
-  {
-    id: 0,
-    cat: 0,
-    done: true,
-    date: "2022-08-05",
-    evaluation: '◎',
-    title: "レバレッジ・リーディング"
-  },
-  {
-    id: 1,
-    cat: 1,
-    done: false,
-    date: "2022-08-05",
-    evaluation: '○',
-    title: "ブログを書く"
-  },
-  {
-    id: 2,
-    cat: 2,
-    done: false,
-    date: "2022-08-06",
-    evaluation: '○',
-    title: "記事を書く（note）"
-  },
+     {
+      "cat": 0,
+      "date": "",
+      "done": false,
+      "evaluation": "",
+      "id": 0,
+      "title": "レバレッジ・リーディング"
+    }
 ];
 
 
